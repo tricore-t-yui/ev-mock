c95800a090dec379e44ba6b070efbb1f76e9d81c
tricore-03 <tricore.as03@gmail.com>
2019-10-21 11:33:09 +0900

    エネミーの視界の判定処理を作成
    エネミーの視界を扇形で判定する処理を作成した。
    当初はスフィアコライダーと扇形の範囲の両方で判定を行うつもりだったが、そもそもコライダーなしで判定できることに気が付いた。
    エネミーの中心からベクトルを伸ばしており、そのベクトルをもとに判定を行っているため、エネミーのオブジェクトが半分以上範囲内に入っていないと視界に入っている扱いにならない。
    
    仮のプレイヤー用にオブジェクトを移動させるだけのテストプログラムを作成した。
    
    【確認ファイル】
    EnemyVisibility.cs
    PlayerMoveTest.cs

---
+++ pro_builder_test/Assets/Mitsumaru/Scripts/PlayerMoverTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// テスト用のプレイヤーの移動処理
+/// （プレイヤーにみたてたオブジェクトを移動させるだけ）
+/// </summary>
+public class PlayerMoverTest : MonoBehaviour
+{
+    // 移動スピード
+    [SerializeField]
+    float speed = 0;
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update()
+    {
+        // 向き
+        Vector3 direction = Vector3.zero;
+
+        // キー入力
+        if (Input.GetKey(KeyCode.UpArrow))    { direction.z++; }
+        if (Input.GetKey(KeyCode.DownArrow))  { direction.z--; }
+        if (Input.GetKey(KeyCode.LeftArrow))  { direction.x--; }
+        if (Input.GetKey(KeyCode.RightArrow)) { direction.x++; }
+
+        // 正規化
+        direction = direction.normalized;
+        // 向きに移動量を加える
+        Vector3 velocity = direction * speed;
+        // 移動する
+        transform.position += velocity;
+    }
+}
