5f65bde833bbdb60258a74ff3a9589fea89ccfb2
tricore-05 <tricore.as05@gamil.com>
2019-10-21 13:48:46 +0900

    息、体力ゲージの点滅処理作成 息、体力ゲージの描画処理を１つにまとめ

---
+++ pro_builder_test/Assets/KazuoFile/GageBlink.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ゲージの点滅クラス
+/// </summary>
+public class GageBlink : MonoBehaviour
+{
+    /// <summary>
+    /// ゲージのタイプ
+    /// </summary>
+    enum GageType
+    {
+        BRETH,
+        HEALTH,
+    }
+
+    [SerializeField]
+    Image image = default;          // 点滅させるゲージの色
+    [SerializeField]
+    Animator brinkAnim = default;   // 点滅アニメーション
+    [SerializeField]
+    GageType type = default;        // ゲージのタイプ
+
+    Color originColor = default;    // 元の色  
+
+    /// <summary>
+    /// 起動処理
+    /// </summary>
+    void OnEnable()
+    {
+        // 起動時に点滅前の色を取ってから点滅開始
+        originColor = image.color;
+        brinkAnim.enabled = true;
+
+        // ゲージのタイプで点滅アニメーションを切り替える
+        switch (type)
+        {
+            case GageType.BRETH : brinkAnim.SetTrigger("Breth");  break;
+            case GageType.HEALTH: brinkAnim.SetTrigger("Health"); break;
+        }
+    }
+
+    /// <summary>
+    /// 停止処理
+    /// </summary>
+    void OnDisable()
+    {
+        // 点滅を終了し、元の色に戻す
+        image.color = originColor;
+        brinkAnim.enabled = false;
+    }
+}
---
+++ pro_builder_test/Assets/KazuoFile/SliderController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ゲージ管理クラス
+/// </summary>
+public class SliderController : MonoBehaviour
+{
+    /// <summary>
+    /// ゲージのタイプ
+    /// </summary>
+    enum SliderType
+    {
+        BRETH,  // 息
+        HEALTH, // 体力
+    }
+
+    [SerializeField]
+    PlayerBrethController brethController = default;    // 息管理クラス
+    [SerializeField]
+    PlayerHealthController healthController = default;  // 体力クラス
+
+    [SerializeField]
+    Slider brethSlider = default;                       // 息ゲージ
+    [SerializeField]
+    Slider healthSlider = default;                      // 体力ゲージ
+
+    [SerializeField]
+    GageBlink brethSliderBlink = default;               // 息ゲージの点滅クラス
+    [SerializeField]
+    GageBlink healthSliderBlink = default;              // 体力ゲージの点滅クラス
+
+    [SerializeField, Range(0, 1)]
+    float blinkPercent = 0;         // 点滅させる基準
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        // 値がが0 ~ 100なので、100で割って0.00 ~ 1.00に変換して値を更新
+        brethSlider.value = brethController.ResidualAmount / 100;
+        healthSlider.value = healthController.Health / 100;
+
+        // 点滅処理
+        Blink();
+    }
+
+    /// <summary>
+    /// 点滅処理
+    /// </summary>
+    void Blink()
+    {
+        // 体力ゲージの値が点滅させる基準を下回ったら点滅開始、上回ったら点滅終了
+        if (healthSlider.value <= blinkPercent)
+        {
+            healthSliderBlink.enabled = true;
+        }
+        else
+        {
+            healthSliderBlink.enabled = false;
+        }
+
+        // 息ゲージは息切れ時も点滅させる、それ以外は同じ
+        if (brethController.IsBreathlessness)
+        {
+            brethSliderBlink.enabled = true;
+        }
+        else if(brethSlider.value <= blinkPercent)
+        {
+            brethSliderBlink.enabled = true;
+        }
+        else
+        {
+            brethSliderBlink.enabled = false;
+        }
+    }
+}
