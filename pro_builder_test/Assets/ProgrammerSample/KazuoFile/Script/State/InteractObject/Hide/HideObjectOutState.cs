c06eddfacfff10933eb49506cd874600bca4fc0b
tricore-as05 <tricore.as05@gmail.com>
2019-11-01 16:35:50 +0900

    隠れるのをやめた時、ベッドやロッカーの脱出方向を向くように修正

--- pro_builder_test/Assets/ProgrammerSample/KazuoFile/Script/State/InteractObject/Hide/HideObjectOutState.cs
+++ pro_builder_test/Assets/ProgrammerSample/KazuoFile/Script/State/InteractObject/Hide/HideObjectOutState.cs
@@ -15,23 +15,42 @@ public class HideObjectOutState : StateMachineBehaviour
     [SerializeField]
     ObjectType type = default;                      // オブジェクトタイプ
     [SerializeField]
+    float exitRotationSpeed = 2;                    // 脱出方向へ向くスピード
+    [SerializeField]
     bool isPlayer = default;                        // プレイヤーかどうか
+
+    Quaternion exitRotation = default;              // 脱出方向
+    bool isExitRotation = false;                    // 脱出方向に向くかどうかのフラグ
 
     /// <summary>
     /// ステートに入った瞬間
     /// </summary>
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // ベットだったらベットの外まで座標移動
+        // フラグを立てる
+        isExitRotation = true;
+
+        // オブジェクトタイプに合わせた、オブジェクトから出る向きを求める
         if (type == ObjectType.BED)
         {
-            // 座標移動はベッドの向きに応じて決める
+            // ベッド
             switch (hideController.HideObjDir)
             {
-                case InteractFunction.DirType.FORWARD: player.rotation = Quaternion.Euler(90, 0, -180);  break;
-                case InteractFunction.DirType.BACK: player.rotation = Quaternion.Euler(90, 0, 0); break;
-                case InteractFunction.DirType.RIGHT: player.rotation = Quaternion.Euler(90, 0, -90); break;
-                case InteractFunction.DirType.LEFT: player.rotation = Quaternion.Euler(90, 0, -270); break;
+                case InteractFunction.DirType.FORWARD: exitRotation = Quaternion.Euler(90, 0, -180); break;
+                case InteractFunction.DirType.BACK: exitRotation = Quaternion.Euler(90, 0, 0); break;
+                case InteractFunction.DirType.RIGHT: exitRotation = Quaternion.Euler(90, 0, -90); break;
+                case InteractFunction.DirType.LEFT: exitRotation = Quaternion.Euler(90, 0, -270); break;
+            }
+        }
+        else
+        {
+            // ロッカー
+            switch (hideController.HideObjDir)
+            {
+                case InteractFunction.DirType.FORWARD: exitRotation = Quaternion.Euler(0, 180, 0); break;
+                case InteractFunction.DirType.BACK: exitRotation = Quaternion.Euler(0, 0, 0); break;
+                case InteractFunction.DirType.RIGHT: exitRotation = Quaternion.Euler(0, 90, 0); break;
+                case InteractFunction.DirType.LEFT: exitRotation = Quaternion.Euler(0, 270, 0); break;
             }
         }
     }
@@ -52,6 +71,18 @@ public class HideObjectOutState : StateMachineBehaviour
                 case InteractFunction.DirType.RIGHT: player.position += Vector3.left * 0.02f; break;
                 case InteractFunction.DirType.LEFT: player.position += Vector3.right * 0.02f; break;
             }
+        }
+
+        // オブジェクトから出る向きに向いていなかったら回転させる
+        if (player.rotation != exitRotation && isExitRotation)
+        {
+            Quaternion rotation = Quaternion.RotateTowards(player.rotation, exitRotation, exitRotationSpeed);
+            player.rotation = rotation;
+        }
+        // 出る向きを向いたらフラグを立てて回転させないようにする
+        else
+        {
+            isExitRotation = false;
         }
     }
 
