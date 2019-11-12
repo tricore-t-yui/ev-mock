ae03eebbf89766e6768ef3f7d762099ddc658694
tricore-03 <tricore.as03@gmail.com>
2019-11-12 10:05:04 +0900

    影人間の通常状態時の動作で「待機型」か「徘徊型」かを選択する処理を作成
    【確認ファイル】
    KageStateNormal.cs
    KageStateStanding.cs

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Enemy/State/KageStateNormal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 影人間のステート：通常状態
+/// </summary>
+public class KageStateNormal : StateMachineBehaviour
+{
+    /// <summary>
+    /// 通常状態時のステートの種類
+    /// </summary>
+    public enum BehaviourKind
+    {
+        Standing = 1,   // 待機
+        Loitering,      // 徘徊
+    }
+
+    // ステートの種類
+    [SerializeField]
+    BehaviourKind behaviourKind = BehaviourKind.Standing;
+
+    /// <summary>
+    /// ステートの開始
+    /// </summary>
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // 指定された状態に変更
+        animator.SetInteger("NormalBehaviourKindId", (int)behaviourKind);
+    }
+}
---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Enemy/State/KageStateStanding.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 影人間のステート：通常状態 / 待機型
+/// </summary>
+public class KageStateStanding : StateMachineBehaviour
+{
+    /// <summary>
+    /// モーションの種類
+    /// </summary>
+    public enum MotionKind
+    {
+        StandBoltUpright = 1,    // 棒立ち
+        LeanAgainstTheWall,      // 壁に寄りかかる
+        HitAgainstTheWall,       // 壁に打ち付ける
+        SleepSprawledOut,        // 大の字になる
+    }
+
+    // 現在のモーション
+    MotionKind currentMotion = default;
+
+    /// <summary>
+    /// ステートの開始
+    /// </summary>
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // モーションの個数分の値からランダムで決定
+        int motionId = Random.Range(1, System.Enum.GetNames(typeof(MotionKind)).Length + 1);
+        // 決定した値をもとにモーションを変更する
+        animator.SetInteger("StandingMotionKindId", motionId);
+    }
+}
