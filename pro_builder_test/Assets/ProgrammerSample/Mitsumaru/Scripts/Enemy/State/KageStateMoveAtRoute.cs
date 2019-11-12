5a020f48004283495c9849154ea7180c85d0ce49
tricore-03 <tricore.as03@gmail.com>
2019-11-12 10:12:26 +0900

    影人間のルートによる徘徊処理を作成
    【確認ファイル】
    KageStateMveAtRoute.cs

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Enemy/State/KageStateMoveAtRoute.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class KageStateMoveAtRoute : StateMachineBehaviour
+{
+    // 移動チェックポイント
+    [SerializeField]
+    MoveCheckPointList checkPointList = default;
+
+    // 現在のチェックポイントのインデックス
+    int currentCheckPointIndex = 0;
+
+    // ナビメッシュ
+    NavMeshAgent navMesh = null;
+
+    // 移動中のカウント
+    int moveCount = 0;
+
+    // 移動スピード
+    [SerializeField]
+    float moveSpeed = 1;
+
+    // 移動の間隔
+    [Space(10)]
+    [SerializeField]
+    int moveInterval = 0;
+
+    /// <summary>
+    /// ステートの開始
+    /// </summary>
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // ナビメッシュのコンポーネントを取得
+        navMesh = navMesh ?? animator.GetComponent<NavMeshAgent>();
+
+        // 移動スピードを設定
+        navMesh.speed = moveSpeed;
+
+        // 最初のチェックポイントを設定
+        navMesh.SetDestination(checkPointList[currentCheckPointIndex]);
+
+        // 移動を開始する
+        animator.SetBool("isLoiteringMove", true);
+    }
+
+    /// <summary>
+    /// ステートの更新
+    /// </summary>
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // 目標のチェックポイントに着いたら
+        if (navMesh.remainingDistance < 0.3f)
+        {
+            // 次のチェックポイントを設定する
+            currentCheckPointIndex = GetNextCheckPointIndex();
+            navMesh.SetDestination(checkPointList[currentCheckPointIndex]);
+        }
+        // 移動中のカウンター
+        moveCount++;
+
+        // 一定時間移動し続けたら
+        if (moveCount >= moveInterval)
+        {
+            // 移動を中断する
+            animator.SetBool("isLoiteringMove", false);
+            // カウンターをリセット
+            moveCount = 0;
+        }
+    }
+
+    /// <summary>
+    /// 次のチェックポイントのインデックスを取得
+    /// </summary>
+    /// <returns></returns>
+    int GetNextCheckPointIndex()
+    {
+        if (currentCheckPointIndex == checkPointList.Count -1)
+        {
+            return 0;
+        }
+        else
+        {
+            return currentCheckPointIndex+1;
+        }
+    }
+}
