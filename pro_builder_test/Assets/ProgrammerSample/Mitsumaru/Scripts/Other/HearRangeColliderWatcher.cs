8e12118452b606fc17f61a0e3aa09bca6e672e04
tricore-03 <tricore.as03@gmail.com>
2019-11-16 16:07:53 +0900

    影人間が音が鳴ったほうへ接近する処理を作成

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Other/HearRangeColliderWatcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 影人間の聴こえる範囲のコライダーを監視する
+/// </summary>
+public class HearRangeColliderWatcher : MonoBehaviour
+{
+    // 対象のタグ名
+    [SerializeField]
+    string targetTagName = default;
+
+    // 物音聴こえた瞬間
+    UnityAction<Collider> onHearEnter;
+
+    /// <summary>
+    /// コールバックのセット
+    /// </summary>
+    /// <param name="call"></param>
+    public void SetCallback(UnityAction<Collider> call)
+    {
+        onHearEnter = call;
+    }
+
+    /// <summary>
+    /// コライダーに当たった瞬間
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other)
+    {
+        // タグが一致したら
+        if (other.tag == targetTagName)
+        {
+            onHearEnter(other);
+        }
+    }
+}
