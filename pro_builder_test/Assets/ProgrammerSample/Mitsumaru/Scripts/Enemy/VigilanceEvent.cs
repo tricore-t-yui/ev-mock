bd46e84a89eaf03518efc86b909068b549c09dd5
tricore-03 <tricore.as03@gmail.com>
2019-11-16 16:12:05 +0900

    視界の範囲内にプレイヤーが入ったら警戒範囲に移行

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Enemy/VigilanceEvent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 警戒状態時のイベント群
+/// </summary>
+public class VigilanceEvent : MonoBehaviour
+{
+    // 警戒状態の開始時
+    [SerializeField]
+    UnityEvent onVigilanceEnter = default;
+
+    // 警戒状態の更新時
+    [SerializeField]
+    UnityEvent onVigilanceUpdate = default;
+
+    // 警戒状態の終了時
+    [SerializeField]
+    UnityEvent onVigilanceExit = default;
+
+    /// <summary>
+    /// 開始時のコールバックを呼ぶ
+    /// </summary>
+    public void OnVigilanceEnter()
+    {
+        onVigilanceEnter.Invoke();
+    }
+
+    /// <summary>
+    /// 更新時のコールバックを呼ぶ
+    /// </summary>
+    public void OnVigilanceUpdate()
+    {
+        onVigilanceUpdate.Invoke();
+    }
+
+    /// <summary>
+    /// 終了時のコールバックを呼ぶ
+    /// </summary>
+    public void OnVigilanceExit()
+    {
+        onVigilanceExit.Invoke();
+    }
+}
