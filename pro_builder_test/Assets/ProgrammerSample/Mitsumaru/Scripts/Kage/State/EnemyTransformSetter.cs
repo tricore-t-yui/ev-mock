145b7ee6c7d705a1ca73db64c8065bfc5965308f
tricore-03 <tricore.as03@gmail.com>
2019-12-07 11:22:37 +0900

    プレイヤークラスに敵のTransformを渡す処理を作成

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Kage/State/EnemyTransformSetter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵のトランスフォームを渡す
+/// </summary>
+public class EnemyTransformSetter : StateMachineBehaviour
+{
+    // ハイドステートコントローラー
+    HideStateController hideStateController = null;
+
+    // 敵の高さ
+    [SerializeField]
+    float enemyHeight = 0;
+
+    /// <summary>
+    /// ステートの開始
+    /// </summary>
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // ハイドステートコントローラーを取得
+        hideStateController = FindObjectOfType<HideStateController>() ?? hideStateController;
+    }
+
+    /// <summary>
+    /// ステートの更新
+    /// </summary>
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // プレイヤーのクラスに敵のトランスフォームを渡す
+        hideStateController.VisibleEnemy(animator.transform, enemyHeight);
+    }
+}
