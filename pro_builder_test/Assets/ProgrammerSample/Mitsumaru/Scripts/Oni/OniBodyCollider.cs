8071c0dbfc64c6eb6d6d1c4d2b11c97e72a72477
tricore-03 <tricore.as03@gmail.com>
2019-12-07 09:39:04 +0900

    鬼関連の処理でプレイヤーへの接近、ハイドポイント付近の探索を作成

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Oni/OniBodyCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 鬼の体のコライダー
+/// </summary>
+public class OniBodyCollider : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator = default;
+
+    class BodyColliderEvent : UnityEvent<Transform,Collision,Animator> { }
+    BodyColliderEvent colliderEvent = new BodyColliderEvent();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        colliderEvent.Invoke(transform.parent.parent, collision, animator);
+    }
+
+    public void Add(UnityAction<Transform,Collision,Animator> call)
+    {
+        colliderEvent.AddListener(call);
+    }
+}
