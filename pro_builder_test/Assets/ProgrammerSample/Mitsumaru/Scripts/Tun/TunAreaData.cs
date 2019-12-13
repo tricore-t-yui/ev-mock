7ffe8f0c9f9a555cb19e687ff51963c0827b9216
tricore-03 <tricore.as03@gmail.com>
2019-12-13 11:49:46 +0900

    不要なファイルを削除

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Tun/TunAreaData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ツンのエリアのデータ
+/// </summary>
+public class TunAreaData : MonoBehaviour
+{
+    // スポーン位置
+    [SerializeField]
+    Vector3 spawnPos = Vector3.zero;
+
+    // ハイドオブジェクト
+    [SerializeField]
+    List<GameObject> hideObject = default;
+
+
+}
