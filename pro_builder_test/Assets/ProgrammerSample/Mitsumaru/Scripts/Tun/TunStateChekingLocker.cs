185b1770e0e8a2d0d48a3899db86021975e7594e
tricore-03 <tricore.as03@gmail.com>
2019-12-17 10:19:02 +0900

    ツン関連のコードのコメント入れ

--- pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Tun/TunStateChekingLocker.cs
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Tun/TunStateChekingLocker.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ロッカーの確認
+/// </summary>
 public class TunStateChekingLocker : StateMachineBehaviour
 {
+    /// <summary>
+    /// ステートの開始
+    /// </summary>
     public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
+        // 既にすべてのハイドポイントを確認済みであれば、その場でツンを消す
         if (animator.GetBool("isHideCheckEnd"))
         {
             animator.gameObject.SetActive(false);
         }
+        // そうでなければ、ハイドポイントの探索開始
         else
         {
             animator.SetBool("isApproachingHide", true);
