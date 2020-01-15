4f2657e5916a710f48e9a7fc08fd13d23058559b
tricore-as05 <tricore.as05@gmail.com>
2020-01-15 18:20:26 +0900

    チュートリアル追加

---
+++ pro_builder_test/Assets/ProgrammerSample/KazuoFile/Script/TutorialTrrigerContoller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// チュートリアル用トリガークラス
+/// </summary>
+public class TutorialTrrigerContoller : MonoBehaviour
+{
+    public bool IsHit { get; private set; } = false; // ヒットフラグ
+
+    /// <summary>
+    /// トリガーが当たったら
+    /// </summary>
+    void OnTriggerEnter(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Player")
+        {
+            IsHit = true;
+        }
+    }
+
+    /// <summary>
+    /// トリガーから離れたら
+    /// </summary>
+    void OnTriggerExit(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Player")
+        {
+            IsHit = false;
+        }
+    }
+}
