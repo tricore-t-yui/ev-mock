345999bb74eb6241eac9de3c4ec1b7c828693677
tricore-as05 <tricore.as05@gmail.com>
2020-01-24 14:19:51 +0900

    テキストシーン更新

---
+++ pro_builder_test/Assets/ProgrammerSample/TextScene/TextController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    [SerializeField]
+    Animator[] texts = default;
+
+    int nowNum = 0;
+    bool isEnd = false;
+
+    /// <summary>
+    /// 起動処理
+    /// </summary>
+    void OnEnable()
+    {
+        nowNum = 0;
+        texts[nowNum].gameObject.transform.parent.gameObject.SetActive(true);
+        isEnd = false;
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isEnd)
+        {
+            texts[nowNum].SetTrigger("Hide");
+            nowNum++;
+            texts[nowNum].gameObject.transform.parent.gameObject.SetActive(true);
+        }
+
+        if(Input.GetMouseButtonDown(1) && nowNum > 0)
+        {
+            texts[nowNum].SetTrigger("Hide");
+            nowNum--;
+            texts[nowNum].gameObject.transform.parent.gameObject.SetActive(true);
+        }
+
+        if (nowNum >= texts.Length - 1)
+        {
+            isEnd = true;
+        }
+        else
+        {
+            isEnd = false;
+        }
+    }
+}
