ef8efa2746a2724a278b303857570969278ea7e0
tricore-03 <tricore.as03@gmail.com>
2020-01-27 17:59:24 +0900

    鬼のリスポーン、デスポーン用のトリガーを作成

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/Scripts/Oni/OniDespawnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OniDespawnTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject oni = default;
+
+    void OnTriggerEnter(Collider other)
+    {
+        oni.SetActive(false);
+    }
+}
