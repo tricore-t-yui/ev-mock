5c50f8017d09cb265784c8d569a056d90cac481c
tricore-03 <tricore.as03@gmail.com>
2020-01-31 13:29:32 +0900

    ホラーモック用の影人間の仕様を実装

---
+++ pro_builder_test/Assets/ProgrammerSample/Mitsumaru/HorrorMock/Scripts/ShadowDespawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowDespawner : StateMachineBehaviour
+{
+    [SerializeField]
+    ParticleSystem particle = default;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        animator.gameObject.SetActive(false);
+    }
+}
