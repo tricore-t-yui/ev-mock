fb1f5559f3a5a56ec51a42510d8335d98ce2354e
tricore-03 <tricore.as03@gmail.com>
2020-02-03 11:45:26 +0900

    ファイル整理

---
+++ pro_builder_test/Assets/MyGameAssets/Scripts/Enemies/Tun/TunStatePlayerChaser.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.AI;
+
+public class TunStatePlayerChaser : StateMachineBehaviour
+{
+    // 移動速度
+    [SerializeField]
+    float speed = 0;
+
+    [SerializeField]
+    float attackRadius = 0;
+
+    // ナビメッシュ
+    NavMeshAgent navMesh = null;
+
+    // プレイヤー
+    GameObject player = null;
+
+    // エリアデータ管理クラス
+    TunAreaDataManager areaDataManager = null;
+
+    // ハイドコントローラー
+    PlayerHideController hideController = null;
+
+    // 敵のサウンドプレイヤー取得
+    EnemySoundPlayer soundPlayer = null;
+
+    TunAreaData currentArea = null;
+
+    /// <summary>
+    /// ステートの開始
+    /// </summary>
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // ナビメッシュを取得
+        navMesh = animator.GetComponent<NavMeshAgent>() ?? navMesh;
+
+        // エリアデータ管理クラスを取得
+        areaDataManager = FindObjectOfType<TunAreaDataManager>() ?? areaDataManager;
+        // ハイドコントローラーを取得
+        hideController = FindObjectOfType<PlayerHideController>() ?? hideController;
+
+        // サウンドプレイヤー
+        soundPlayer = animator.GetComponentInChildren<EnemySoundPlayer>() ?? soundPlayer;
+
+        soundPlayer.Play("Mitsuketa");
+
+        // 現在のエリアを取得
+        currentArea = areaDataManager.GetTunAreaData(hideController.HideObj.GetInstanceID());
+
+        // 移動速度をセット
+        navMesh.speed = speed;
+
+        navMesh.isStopped = false;
+
+        player = GameObject.Find("Player") ?? player;
+
+        animator.SetFloat("moveSpeed", navMesh.speed);
+    }
+
+    /// <summary>
+    /// プレイヤーの更新
+    /// </summary>
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        navMesh.isStopped = false;
+        // プレイヤーの位置を取得する
+        navMesh.SetDestination(player.transform.position);
+
+        if (navMesh.remainingDistance < attackRadius)
+        {
+            animator.SetTrigger("attackStart");
+        }
+
+        if (!currentArea.IsBoudsContains(player.transform.position))
+        {
+            animator.gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// ステートの終了
+    /// </summary>
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) { }
+}
