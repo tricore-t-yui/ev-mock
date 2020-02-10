63e27c93a30ea7191a2cda3d8c31909f59064117
tricore-as05 <tricore.as05@gmail.com>
2020-02-10 11:06:30 +0900

    トリガーに触れると音発生、トリガーに触ったら指定の絵が全画面で指定時間出て、フェードアウトする作成

---
+++ pro_builder_test/Assets/MyGameAssets/Scripts/Others/TriggerEffectController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// トリガーの画面エフェクト管理クラス
+/// </summary>
+public class TriggerEffectController : MonoBehaviour
+{
+    [SerializeField]
+    TriggerEffectSpawner spawner = default;    // トリガーエフェクトのスポナー
+    [SerializeField]
+    Image image = default;                     // トリガーエフェクト
+    [SerializeField]
+    float waitSecond = 3;                      // フェードアウトするまでの時間
+
+    float alpha = 0;                           // トリガーエフェクトのアルファ値
+
+    /// <summary>
+    /// 起動処理
+    /// </summary>
+    void OnEnable()
+    {
+        // 初期化してフェードイン開始
+        image.sprite = spawner.displaySprite;
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+        alpha = 0;
+
+        StartCoroutine(FadeInTriggerEffect());
+    }
+
+    /// <summary>
+    /// フェードインのコルーチン
+    /// </summary>
+    IEnumerator FadeInTriggerEffect()
+    {
+        while (alpha < 1)
+        {
+            image.color += new Color(0, 0, 0, 0.01f);
+            alpha += 0.01f;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(waitSecond);
+
+        StartCoroutine(FadeOutTriggerEffect());
+    }
+
+    /// <summary>
+    /// フェードアウトのコルーチン
+    /// </summary>
+    IEnumerator FadeOutTriggerEffect()
+    {
+        while (alpha > 0)
+        {
+            image.color -= new Color(0, 0, 0, 0.01f);
+            alpha -= 0.01f;
+            yield return null;
+        }
+
+        // フェードアウトし終わったら消す
+        gameObject.SetActive(false);
+    }
+}
---
+++ pro_builder_test/Assets/MyGameAssets/Scripts/Others/TriggerEffectSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathologicalGames;
+
+/// <summary>
+/// トリガーエフェクトのスポナー
+/// </summary>
+public class TriggerEffectSpawner : MonoBehaviour
+{
+    /// <summary>
+    /// エフェクトのタイプ
+    /// </summary>
+    public enum EffectType
+    {
+        SAMPLE1,
+        SAMPLE2,
+        SAMPLE3,
+    }
+
+    [SerializeField]
+    GameObject spawnBase = default;                               // 複製元
+    [SerializeField]
+    Sprite[] tirggerEffect = default;                             // 表示される画像達
+
+    public Sprite displaySprite { get; private set; } = default;  // 表示する画像
+    List<Transform> spawnList = new List<Transform>();            // スポーンされたオブジェクトのリスト
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        Despawn();
+    }
+
+    /// <summary>
+    /// デスポーン
+    /// </summary>
+    void Despawn()
+    {
+        List<Transform> DespawnList = new List<Transform>();      // スポーンされたオブジェクトのリスト
+
+        foreach (var item in spawnList)
+        {
+            // デスポーンして、リストから削除
+            if (!item.gameObject.activeInHierarchy)
+            {
+                PoolManager.Pools["Sound"].Despawn(item);
+                DespawnList.Add(item);
+            }
+        }
+
+        foreach (var item in DespawnList)
+        {
+            spawnList.Remove(item);
+        }
+    }
+
+    /// <summary>
+    /// 再生
+    /// </summary>
+    public void Spawn(EffectType type)
+    {
+        // 再生される音をセット
+        displaySprite = tirggerEffect[(int)type];
+
+        // スポーンしてリストに追加
+        var spawn = PoolManager.Pools["Sound"].Spawn(spawnBase);
+        spawnList.Add(spawn);
+    }
+}
