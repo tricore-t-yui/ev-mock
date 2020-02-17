41ad34c0e9d0cd6d424210daae165ae1634c832c
tricore-03 <tricore.as03@gmail.com>
2020-02-17 17:49:53 +0900

    敵のパラメータの説明文を追加

--- pro_builder_test/Assets/MyGameAssets/Scripts/Enemies/HorrorMock_Shadow/EnemyParameter.cs
+++ pro_builder_test/Assets/MyGameAssets/Scripts/Enemies/HorrorMock_Shadow/EnemyParameter.cs
@@ -70,7 +70,6 @@ public class EnemyParameter : MonoBehaviour
     SectorCollider attackRange = default;
 
     [Title("[Range Parameters(範囲系パラメータ)]")]
-
     [SerializeField]
     [ListDrawerSettings(HideAddButton = true,HideRemoveButton = true,Expanded = true,ShowItemCount = false,ShowPaging = false,DraggableItems = false)]
     RangeParameter[] stateRanges = new RangeParameter[3]
@@ -81,7 +80,7 @@ public class EnemyParameter : MonoBehaviour
     };
     public RangeParameter[] StateRanges => stateRanges;
 
-    [Header("View Range")]
+    [Header("View Range(視野範囲(緑扇形)の設定)")]
     [Tooltip("視野範囲の角度を指定します。")]
     [SerializeField]
     float viewAngle = 120;
@@ -91,7 +90,7 @@ public class EnemyParameter : MonoBehaviour
     float viewDistance = 1;
     public float ViewDistance => viewDistance;
 
-    [Header("Attack Range")]
+    [Header("Attack Range(戦闘範囲(赤扇形)の設定)")]
     [Tooltip("攻撃範囲の角度を指定します。")]
     [SerializeField]
     float attackAngle = 120;
@@ -116,7 +115,7 @@ public class EnemyParameter : MonoBehaviour
 
     [Space(10)]
     [ShowIf("IsNormalStateWanderer")]
-    [Tooltip("徘徊の種類を指定します。")]
+    [Tooltip("徘徊の種類を指定します。ルート徘徊orランダム徘徊")]
     [SerializeField]
     WandererType wandererType = WandererType.Route;
     public WandererType Wanderer => wandererType;
@@ -155,25 +154,39 @@ public class EnemyParameter : MonoBehaviour
 
     [Space(10)]
     [SerializeField]
+    [Tooltip("フェードの反転を行うかどうかを設定します" +
+        "\nオフ：出現範囲に触れると現れて、離れると消える。" +
+        "\nオン：出現範囲に触れると消えて、離れると現れる。")]
     bool inverse = false;
     public bool Inverse => inverse;
 
     [SerializeField]
+    [Tooltip("距離によって透明度を変えるかどうかを設定します。" +
+        "\nオフ：出現範囲に触れると指定の速度でじんわり現れる。" +
+        "\nオン：出現範囲に触れた状態からプレイヤーの距離によって透明度が変わる。")]
     bool isTransparencyByDistance = false;
     public bool IsTransparencyByDistance => isTransparencyByDistance;
 
-    [Tooltip("姿が現れるときのフェードを設定します。\n値が小さいほど滑らかに現れます。")]
+    [Tooltip("姿が現れるときの速度を設定します。" +
+        "\n値が小さいほど、ゆっくりじんわり現れます。")]
+    [HideIf("isTransparencyByDistance")]
     [SerializeField]
     [Range(0, 1)]
     float appearFadeTime = 0.1f;
     public float AppearFadeTime => appearFadeTime;
 
     [SerializeField]
+    [Tooltip("透明度の最小値を設定します。" +
+        "\nどんなに離れてもこの値より透明にはなりません。")]
+    [ShowIf("isTransparencyByDistance")]
     [Range(0, 1)]
     float transparencyMin = 0.5f;
     public float TransparencyMin => transparencyMin;
 
     [SerializeField]
+    [Tooltip("透明度の最大値を設定します。" +
+        "\nどんなに近づいてもこの値よりは濃くなりません。")]
+    [ShowIf("isTransparencyByDistance")]
     [Range(0, 1)]
     float transparencyMax = 1;
     public float TransparencyMax => transparencyMax;
@@ -184,12 +197,13 @@ public class EnemyParameter : MonoBehaviour
     float safeSoundLevelMax = 5;
     public float SafeSoundLevelMax => safeSoundLevelMax;
 
-    [Tooltip("最初に音を聞いたときに、警戒に移行するまでの時間を設定します。")]
+    [Tooltip("最初に音を聞いたときに、警戒に移行するまでの時間を設定します。" +
+        "\nこの時間までに警戒範囲から離れれば気づかれません。")]
     [SerializeField]
     float safeTime = 1;
     public float SafeTime => safeTime;
 
-    [Tooltip("姿が見えていない状態で音を検知できるかどうかを設定します。\nオンの場合は姿が見えていなくても音を検知すれば警戒に移行して追ってきます。")]
+    [Tooltip("姿が見えていない状態で音を検知できるかどうかを設定します。\nオンの場合は姿が見えていなくてもプレイヤーが発した音を検知すれば警戒に移行して迫ってきます。")]
     [SerializeField]
     bool isDetectNoiseToTransparent = false;
     public bool IsDetectNoiseToTransparent => isDetectNoiseToTransparent;
@@ -197,34 +211,47 @@ public class EnemyParameter : MonoBehaviour
 
     [Space(15)]
     [Title("[State : Caution(警戒状態)]")]
+    [Tooltip("警戒状態時の待機時間を設定します。" +
+        "\n音を聞いて近づいたが、結局プレイヤーを見つけられなかったときにこの時間でしばらく待機を行います。")]
     [SerializeField]
     float cautionWaitTime = 0;
     public float CautionWaitTime => cautionWaitTime;
 
     [SerializeField]
+    [Tooltip("警戒状態時の移動速度を設定します。" +
+        "\nプレイヤーが発した音を聞いたときはこの速度で近づいてきます。")]
     float cautionMoveSpeed = 1.5f;
     public float CautionMoveSpeed => cautionMoveSpeed;
 
     [SerializeField]
+    [Tooltip("プレイヤーを見失ったり音を聞きとれなくなったときに、もとの徘徊ルートや待機位置に戻ります。" +
+        "\n戻る際にもとの位置との距離がこの値よりも遠かったときは瞬間移動を行います。")]
     float returnWarpDistance = 1;
     public float ReturnWarpDistance => returnWarpDistance;
 
     [Space(15)]
     [Title("[State : Fighting(戦闘状態)]")]
     [SerializeField]
+    [Tooltip("戦闘状態時の待機時間を設定します。" +
+        "\nプレイヤーを途中で見失ったときにこの時間で待機を行います。")]
     float fightingWaitTime = 0;
     public float FightingWaitTime => fightingWaitTime;
 
     [SerializeField]
+    [Tooltip("攻撃後の待機時間を設定します。" +
+        "\nプレイヤーを攻撃したあとはこの時間でしばらく待機します。")]
     float attackedWaitTime = 0;
     public float AttackedWaitTime => attackedWaitTime;
 
     [SerializeField]
+    [Tooltip("戦闘状態時の移動速度を設定します。" +
+        "\nこの速度でプレイヤーを追いかけます。")]
     float fightingMoveSpeed = 2;
     public float FightingMoveSpeed => fightingMoveSpeed;    
 
     [SerializeField]
     [Range(0,100)]
+    [Tooltip("プレイヤーに与えるダメージ値を設定します。")]
     float damage = 0;
     public float Damage => damage;
 
@@ -238,41 +265,44 @@ public class EnemyParameter : MonoBehaviour
     [Title("[Special Action Settings(特殊アクション)]")]
 
     [SerializeField]
+    [Tooltip("このフラグがオンの場合、攻撃してしばらく待機した後に消えます。")]
     bool isAttackedDisappear = false;
     public bool IsAttackedDisappear => isAttackedDisappear;
 
     [SerializeField]
+    [Tooltip("このフラグがオンの場合、一定距離近づくと消えます。")]
     bool isApproachedDisappear = false;
     public bool IsApproachedDisappear => isApproachedDisappear;
 
     [SerializeField]
+    [Tooltip("このフラグがオンの場合、カメラから外れた瞬間消えます。" +
+        "\n注意：Unityの仕様でSceneビューとGameビューの両方のカメラから外れたときに動作します。" +
+        "\nなので、対象の敵がSceneビューから見えない状態で試すようにしてください。")]
     bool isCameraFadeOutDisappear = false;
     public bool IsCameraFadeOutDisappear => isCameraFadeOutDisappear;
 
     [SerializeField]
+    [Tooltip("上記のアクションの消えたときに初期位置にリスポーンさせるかどうかを設定します。")]
     bool isRespawn = false;
     public bool IsRespawn => isRespawn;
 
     [SerializeField]
+    [Tooltip("どのくらい近づいたら消えるかを設定します。" +
+        "\n敵にこの距離よりも近づくと消えます。")]
     float disappearDistance = 0;
     public float DisappearDistance => disappearDistance;
-
-    [Space(5)]
-    [SerializeField]
-    bool isNotAttackApproached = false;
-    public bool IsNotAttackApproached => isNotAttackApproached;
-
-    [SerializeField]
-    float approachedDistance = 1;
-    public float ApproachedDistance => approachedDistance;
 
     [Space(15)]
     [Title("[Other Settings(その他)]")]
     [SerializeField]
+    [Tooltip("敵の状態遷移を行うかどうかを設定します。" +
+        "\nこのフラグがオンの場合は状態遷移が一切行われなくなります。")]
     bool isStaticState = false;
     public bool IsStaticState => isStaticState;
 
     [SerializeField]
+    [Tooltip("ゲーム開始時に自動でスポーンさせるか設定します。" +
+        "\nオフの場合は自動では出現しなくなります。")]
     bool isInitialAutoSpawn = true;
 
     // 初期位置
