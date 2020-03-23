c7780fee8227b9ce21cd4e5678a3527e2364929e
Takeru Yui <t.yui@tri-core.co.jp>
2020-03-23 11:34:54 +0900

    ステージクリアと後半ワープを実装

---
+++ pro_builder_test/Assets/MyGameAssets/Scripts/Others/DebugStageClear.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugStageClear : MonoBehaviour
+{
+    public GameObject clearObject;
+    public List<GameObject> goalOffList;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+
+        var onis = Object.FindObjectsOfType(typeof(Oni));
+        bool clear = false;
+        foreach (var item in onis)
+        {
+            var gameObj = (Oni)item;
+            if(gameObj != null)
+            {
+                gameObj.gameObject.SetActive(false);
+                clear = true;
+            }
+        }
+        if(clear)
+        {
+            foreach (var item in goalOffList)
+            {
+                item.SetActive(false);
+            }
+            clearObject.SetActive(true);
+        }
+    }
+}
---
+++ pro_builder_test/Assets/MyGameAssets/Scripts/Others/DebugWarpAndOnOff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DebugWarpAndOnOff : MonoBehaviour
+{
+    public Transform warpPoint;
+    public Transform target;
+    public List<GameObject> onObj;
+    public List<GameObject> offObj;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+
+        target.transform.position = warpPoint.position;
+        foreach (var item in onObj)
+        {
+            item.SetActive(true);
+        }
+        foreach (var item in offObj)
+        {
+            item.SetActive(false);
+        }
+    }
+}
